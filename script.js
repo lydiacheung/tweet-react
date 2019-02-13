84bbdc1ec19c088116fd19d2e6ccd2a6ecca2f28
Lydia Cheung <lydiacheung@Lydias-MacBook-Air.local>
2019-02-13 22:46:40 +0800

    mvp

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-var monkey = "hello";
+class Tweet extends React.Component {
+  render() {
+    let tweetElements = this.props.tweets.map(tweet => {
+      return (
+        <div>
+          <img src={tweet.user.profile_image_url} />
+          KANYE WEST@ {tweet.user.screen_name}
+          <br />
+          <br />
+          {tweet.text}
+          <br />
+          <br />
+          Retweets:{tweet.retweet_count}&nbsp; Favorites:{tweet.favorite_count}
+          <hr />
+        </div>
+      );
+    });
+
+    return <ul>{tweetElements}</ul>;
+  }
+}
+
 ReactDOM.render(
-    <div>
-      {monkey}
-    </div>,
-    document.getElementById('root')
+  <Tweet tweets={Tweets} />,
+
+  document.getElementById("root")
 );
